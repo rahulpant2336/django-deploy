295cb61d45bc0a58542abe51ab8f04f2317b7261
Rahul Pant <44518090+rahulpant2336@users.noreply.github.com>
2019-10-28 16:10:36 +0530

    Update settings.py

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+heroku config:set DISABLE_COLLECTSTATIC=1
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
